d0faabd7034a7543e40daa72be1af90d41d88fc3
Roshandelpoor Mohammad <roshandelpoor@outlook.com>
2025-04-17 18:30:24 +0330

    Redis job test coverage

--- tests/Queue/QueueRedisJobTest.php
+++ tests/Queue/QueueRedisJobTest.php
@@ -43,6 +43,57 @@ class QueueRedisJobTest extends TestCase
         $job->release(1);
     }
 
+    public function testAttemptsReturnsCorrectValue()
+    {
+        $job = $this->getJob();
+        $this->assertEquals(2, $job->attempts());
+    }
+
+    public function testGetJobIdReturnsNullWhenNoId()
+    {
+        $job = $this->getJob();
+        $this->assertNull($job->getJobId());
+    }
+
+    public function testGetRawBodyReturnsCorrectPayload()
+    {
+        $job = $this->getJob();
+        $expectedPayload = json_encode(['job' => 'foo', 'data' => ['data'], 'attempts' => 1]);
+        $this->assertEquals($expectedPayload, $job->getRawBody());
+    }
+
+    public function testGetReservedJobReturnsCorrectPayload()
+    {
+        $job = $this->getJob();
+        $expectedPayload = json_encode(['job' => 'foo', 'data' => ['data'], 'attempts' => 2]);
+        $this->assertEquals($expectedPayload, $job->getReservedJob());
+    }
+
+    public function testGetRedisQueueReturnsCorrectInstance()
+    {
+        $job = $this->getJob();
+        $this->assertInstanceOf(\Illuminate\Queue\RedisQueue::class, $job->getRedisQueue());
+    }
+
+    public function testFireProperlyCallsTheJobHandlerWithFailedMethod()
+    {
+        $job = $this->getJob();
+        $job->getContainer()->shouldReceive('make')->once()->with('foo')->andReturn($handler = m::mock(stdClass::class));
+        $handler->shouldReceive('fire')->once()->with($job, ['data']);
+        $handler->shouldReceive('failed')->never();
+
+        $job->fire();
+    }
+
+    public function testJobCanBeDeletedAndReleased()
+    {
+        $job = $this->getJob();
+        $job->getRedisQueue()->shouldReceive('deleteAndRelease')->once()
+            ->with('default', $job, 0);
+
+        $job->release();
+    }
+
     protected function getJob()
     {
         return new RedisJob(
