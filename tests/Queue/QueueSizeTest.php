aae8bc5884d490e00e7cd75878092077dcda26f6
Roshandelpoor Mohammad <roshandelpoor@outlook.com>
2025-04-17 18:44:23 +0330

    enhance Queue size test coverage

--- tests/Queue/QueueSizeTest.php
+++ tests/Queue/QueueSizeTest.php
@@ -25,6 +25,55 @@ class QueueSizeTest extends TestCase
         $this->assertEquals(2, Queue::size());
         $this->assertEquals(1, Queue::size('Q2'));
     }
+
+    public function test_multiple_queues_size()
+    {
+        Queue::fake();
+
+        dispatch(new TestJob1)->onQueue('queue1');
+        dispatch(new TestJob2)->onQueue('queue2');
+        dispatch(new TestJob1)->onQueue('queue1');
+        dispatch(new TestJob2)->onQueue('queue3');
+
+        $this->assertEquals(2, Queue::size('queue1'));
+        $this->assertEquals(1, Queue::size('queue2'));
+        $this->assertEquals(1, Queue::size('queue3'));
+
+        $this->assertEquals(0, Queue::size());
+    }
+
+    public function test_empty_queue_after_jobs_processed()
+    {
+        Queue::fake();
+
+        dispatch(new TestJob1);
+        dispatch(new TestJob2);
+
+        $this->assertEquals(2, Queue::size());
+
+        Queue::assertPushed(TestJob1::class);
+        Queue::assertPushed(TestJob2::class);
+
+        Queue::assertPushed(TestJob1::class, function ($job) {
+            return true;
+        });
+        Queue::assertPushed(TestJob2::class, function ($job) {
+            return true;
+        });
+    }
+
+    public function test_queue_size_with_delayed_jobs()
+    {
+        Queue::fake();
+
+        dispatch(new TestJob1)->delay(now()->addMinutes(5));
+        dispatch(new TestJob2)->delay(now()->addMinutes(10));
+        dispatch(new TestJob1);
+
+        $this->assertEquals(3, Queue::size());
+        Queue::assertPushed(TestJob1::class, 2);
+        Queue::assertPushed(TestJob2::class, 1);
+    }
 }
 
 class TestJob1 implements ShouldQueue
