520b783bd2e5eaa5d369cff29a58450c59a7a7c7
Roshandelpoor Mohammad <roshandelpoor@outlook.com>
2025-04-17 19:15:40 +0330

    enhance ValidationRequiredIf test coverage

--- tests/Validation/ValidationRequiredIfTest.php
+++ tests/Validation/ValidationRequiredIfTest.php
@@ -78,4 +78,52 @@ class ValidationRequiredIfTest extends TestCase
         $v = new Validator($trans, ['x' => 'foo'], ['x' => ['string', $rule]]);
         $this->assertTrue($v->passes());
     }
+
+    public function testRequiredIfWithNullValue()
+    {
+        $trans = new Translator(new ArrayLoader, 'en');
+        $rule = new RequiredIf(true);
+
+        $v = new Validator($trans, ['x' => null], ['x' => $rule]);
+        $this->assertTrue($v->fails());
+    }
+
+    public function testRequiredIfWithCallableReturningNull()
+    {
+        $trans = new Translator(new ArrayLoader, 'en');
+        $rule = new RequiredIf(function() {
+            return null;
+        });
+
+        $v = new Validator($trans, ['x' => 'foo'], ['x' => $rule]);
+        $this->assertTrue($v->passes());
+    }
+
+    public function testRequiredIfWithMultipleRulesWhenRequired()
+    {
+        $trans = new Translator(new ArrayLoader, 'en');
+        $rule = new RequiredIf(true);
+
+        $v = new Validator($trans, [], ['x' => ['numeric', $rule]]);
+        $this->assertTrue($v->fails());
+
+        $v = new Validator($trans, ['x' => 'not-a-number'], ['x' => ['numeric', $rule]]);
+        $this->assertTrue($v->fails());
+    }
+
+    public function testRequiredIfWithDynamicCondition()
+    {
+        $trans = new Translator(new ArrayLoader, 'en');
+        $someValue = 'test';
+        
+        $rule = new RequiredIf(function() use ($someValue) {
+            return $someValue === 'test';
+        });
+
+        $v = new Validator($trans, [], ['x' => $rule]);
+        $this->assertTrue($v->fails());
+
+        $v = new Validator($trans, ['x' => 'value'], ['x' => $rule]);
+        $this->assertTrue($v->passes());
+    }
 }
